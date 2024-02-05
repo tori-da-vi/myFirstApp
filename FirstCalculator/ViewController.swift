2007da53995ff000bd85789dbfba49933b6e2c84
Victoria Chelnokova <chelnokovavika@yandex.ru>
2024-02-05 15:06:13 +0300

    first commit

---
+++ FirstCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstCalculator
+//
+//  Created by Victoria Chelnokova on 04.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("вау, я программистка")
+    }
+    
+
+}
+
